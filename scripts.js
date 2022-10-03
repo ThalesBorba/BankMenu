789aed58f2b1a419622ad9893fa62621713cc04f
ThalesBorba <thales_borba@hotmail.com>
2022-10-02 22:40:04 -0300

    Update

--- scripts.js
+++ scripts.js
@@ -6,13 +6,3 @@ function hideBalance() {
         balance.style.display = "none";
       }
 }
-
-function adaptButton() {
-  var buttons = document.getElementsByClassName("square")
-
-  buttons.forEach(button => {
-    button.style.height = button.style.width
-  });
-}
-
-window.onresize = adaptButton
